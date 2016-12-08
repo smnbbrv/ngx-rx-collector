a025cefeb5d0af6aab70696888dc789390706e7c
Philip Steinebrunner <p.steinebrunner@ibitech.com>
2016-12-08 08:10:00 +0100

    Instantiate CollectorEvent within Collected decorator

--- app/src/decorator.ts
+++ app/src/decorator.ts
@@ -4,14 +4,20 @@ export class CollectorEvent extends Subject<void> {}
 
 export function Collected() {
   return (prototype: any, name: string) => {
-    let onDestroy = prototype.ngOnDestroy;
+    const onDestroy = prototype.ngOnDestroy;
+    const event = new CollectorEvent();
+
+    Object.defineProperty(prototype, name, {
+      configurable: false,
+      get: () => event
+    });
 
     prototype.ngOnDestroy = function() {
       if (onDestroy) {
         onDestroy.call(this);
       }
 
-      this[name].next();
+      event.next();
     };
   };
 };
